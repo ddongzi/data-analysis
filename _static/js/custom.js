dd810afabffb665f8d44c93f0cbf4d5c57d0effc
ddongzi <103313356+ddongzi@users.noreply.github.com>
2026-02-13 15:47:02 +0000

    Deploying to gh-pages from @ ddongzi/data-analysis@adbf96cf69b216c56bce98082f911fdd3097032c 🚀

--- _static/js/custom.js
+++
@@ -1,6 +0,0 @@
-// 输入区域配合sphinx_togglebutton 折叠
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".cell_input").forEach(function(cell) {
-        cell.classList.add("hide_input");
-    });
-});
