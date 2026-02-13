e9e73d2e254b7de481e4d49c8495896f2542dc47
dong <635176614@qq.com>
2026-02-13 17:01:33 +0800

    1

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'zh_CN'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
